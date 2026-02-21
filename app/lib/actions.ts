ef4c14087e6a11a4fc6bafa0905e4b64b49ffa99
aibekzhumabekov <aibek.zhumabekov@proton.me>
2026-02-20 22:40:03 -0600

    Fix edit invoice link, deleteInvoice error handling, and add form accessibility attributes

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -110,11 +110,10 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
 export async function deleteInvoice(id: string) {
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
   } catch (error) {
-    console.error('Database Error: Failed to Delete Invoice.', error);
-    throw new Error('Database Error: Failed to Delete Invoice.');
+    return { message: 'Database Error: Failed to Delete Invoice.' };
   }
-  revalidatePath('/dashboard/invoices');
 }
 
 export async function authenticate(
